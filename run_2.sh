2df2adb5863759815d544c857ab5063643e80a1b
vincent05r <111105725+vincent05r@users.noreply.github.com>
2024-04-05 21:14:49 +1100

    new dataset ready

---
+++ run_2.sh
@@ -0,0 +1,8 @@
+# Test Period 2016, Sequence Length 10
+python main.py --root_path data/EcmP_stock_L_2016_24_dva_spec --sequence_length 10
+# Test Period 2016, Sequence Length 20
+python main.py --root_path data/EcmP_stock_L_2016_24_dva_spec --sequence_length 20
+# Test Period 2016, Sequence Length 40
+python main.py --root_path data/EcmP_stock_L_2016_24_dva_spec --sequence_length 40
+# Test Period 2016, Sequence Length 60
+python main.py --root_path data/EcmP_stock_L_2016_24_dva_spec --sequence_length 60
